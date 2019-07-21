487d36c6cd55a2d440813ff5afdffc37207f3369
Peter Pallen <peter@ppallen.be>
2019-07-21 17:05:30 +0200

    Setup base file with home and about routes
    Signed-off-by: Peter Pallen <peter@ppallen.be>

---
+++ flaskblog.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask.templating import render_template
+
+
+app = Flask(__name__)
+posts = [
+    {
+        "author": "Peter Pallen",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "July 20, 2019",
+    },
+    {
+        "author": "Carine Pallen",
+        "title": "Blog post 2",
+        "content": "Second post content",
+        "date_posted": "July 21, 2019",
+    },
+]
+
+
+@app.route("/")
+@app.route("/home")
+@app.route("/index")
+def home():
+    return render_template("home.html.j2", posts=posts, title="Home")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html.j2", title="About")
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", debug=True)
