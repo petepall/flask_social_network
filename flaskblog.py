363eb85a38ae6e0ec0baf40314b014ffaa4be9bd
Peter Pallen <peter@ppallen.be>
2019-07-21 18:41:14 +0200

    Add routes for login and registration
    Signed-off-by: Peter Pallen <peter@ppallen.be>

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask.helpers import url_for
 from flask.templating import render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "956b91dcad619e1d5137ee2fc1e14bc2"
+
 posts = [
     {
         "author": "Peter Pallen",
@@ -29,6 +32,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html.j2", title="About")
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html.j2", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html.j2", title="Login", form=form)
 
 
 if __name__ == "__main__":
