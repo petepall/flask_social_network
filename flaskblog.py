3c8430dd5c843462ab17b39ee34840af3c3ac1b7
Peter Pallen <peter@ppallen.be>
2019-07-21 19:31:21 +0200

    added flash messages and dummy login validation
    Signed-off-by: Peter Pallen <peter@ppallen.be>

--- flaskblog.py
+++ flaskblog.py
@@ -39,14 +39,26 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
+        flash(f"Account created for {form.username.data.upper()}!", "success")
         return redirect(url_for("home"))
     return render_template("register.html.j2", title="Register", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if (
+            form.email.data == "admin@blog.com"
+            and form.password.data == "strike"
+        ):
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash(
+                "Login unsuccessful. Please check username and password",
+                "danger",
+            )
     return render_template("login.html.j2", title="Login", form=form)
 
 
