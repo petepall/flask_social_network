e430910a87945424d2b8a2c5e1e7a3079b92da3d
Peter Pallen <peter@ppallen.be>
2019-07-21 20:49:18 +0200

    reorg into package
    Signed-off-by: Peter Pallen <peter@ppallen.be>

---
+++ flaskblog/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "956b91dcad619e1d5137ee2fc1e14bc2"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/site.db"
+db = SQLAlchemy(app)
+
+from flaskblog import routes
