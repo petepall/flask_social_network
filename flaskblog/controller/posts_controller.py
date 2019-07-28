64c00648871d98403315071b0fe4178d0b68b5c1
Peter Pallen <peter@ppallen.be>
2019-07-28 20:29:10 +0200

    Add post update functionality forms
    Signed-off-by: Peter Pallen <peter@ppallen.be>

--- flaskblog/controller/posts_controller.py
+++ flaskblog/controller/posts_controller.py
@@ -1,4 +1,4 @@
-from flask import redirect
+from flask import abort, redirect, request
 from flask.helpers import flash, url_for
 from flask.templating import render_template
 from flask_login import current_user, login_required
@@ -22,4 +22,38 @@ def new_post():
         db.session.commit()
         flash("Your post has been created!", "success")
         return redirect(url_for("home"))
-    return render_template("create_post.html.j2", title="New post", form=form)
+    return render_template(
+        "create_post.html.j2", title="New post", form=form, legend="New post"
+    )
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html.j2", title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template(
+        "create_post.html.j2",
+        title="Update post",
+        form=form,
+        legend="Update post",
+    )
