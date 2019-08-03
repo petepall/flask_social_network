c32262ba444ec558f19f1e3d9c6ee04a874cacf5
Peter Pallen <peter@ppallen.be>
2019-08-03 18:35:35 +0200

    Adding pagination to home and general_controller
    Signed-off-by: Peter Pallen <peter@ppallen.be>

--- flaskblog/controller/general_controller.py
+++ flaskblog/controller/general_controller.py
@@ -1,3 +1,4 @@
+from flask.globals import request
 from flask.templating import render_template
 
 from flaskblog import app
@@ -8,7 +9,8 @@ from flaskblog.models.post_model import Post
 @app.route("/home")
 @app.route("/index")
 def home():
-    posts = Post.query.all()
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template("home.html.j2", posts=posts, title="Home")
 
 
