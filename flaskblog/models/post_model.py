f32b21ac8b3d89d94b995b11d0f9f0b53ef0184b
Peter Pallen <peter@ppallen.be>
2019-08-18 21:41:31 +0200

    added docstrings
    Signed-off-by: Peter Pallen <peter@ppallen.be>

--- flaskblog/models/post_model.py
+++ flaskblog/models/post_model.py
@@ -4,6 +4,15 @@ from flaskblog import db
 
 
 class Post(db.Model):
+    """Class representing the Post table in the database
+
+    Parameters
+    ----------
+    db : database object
+
+        represents the database object.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(
@@ -13,4 +22,6 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     def __repr__(self):
+        """String representation of the post object
+        """
         return f"Post('{self.title}', '{self.date_posted}')"
--- flaskblog/models/user_model.py
+++ flaskblog/models/user_model.py
@@ -7,10 +7,31 @@ from flask import current_app
 
 @login_manager.user_loader
 def load_user(user_id):
+    """Load the current user's id
+
+    Parameters
+    ----------
+    user_id : int
+
+        ID of the current connected user
+    """
     return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
+    """Class representing the User table in the database
+
+    Parameters
+    ----------
+    db : Database object
+
+        Represents the database model base class
+
+    UserMixin : Flask user management object
+
+        Enables Flask user management functions
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -21,14 +42,32 @@ class User(db.Model, UserMixin):
     posts = db.relationship("Post", backref="author", lazy=True)
 
     def __repr__(self):
+        """String representation for the User
+        """
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
     def get_reset_token(self, expires_seconds=1800):
+        """Get a token for user password reset, that is send by email
+
+        Parameters
+        ----------
+        expires_seconds : int, optional
+
+            delay in seconds after which the token expires, by default 1800
+        """
         s = Serializer(current_app.config["SECRET_KEY"], expires_seconds)
         return s.dumps({"user_id": self.id}).decode("utf-8")
 
     @staticmethod
     def verify_reset_token(token):
+        """Verify if a given token is valid
+
+        Parameters
+        ----------
+        token : token
+
+            Token to verify
+        """
         s = Serializer(current_app.config["SECRET_KEY"])
         try:
             user_id = s.loads(token)["user_id"]
